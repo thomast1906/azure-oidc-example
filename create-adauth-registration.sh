945cf8d723be11b2bb351651fed323f952b453bb
Thomas Thornton <>
2025-02-25 08:37:13 +0000

    azcli v2

--- create-adauth-registration.sh
+++ create-adauth-registration.sh
@@ -1,6 +1,5 @@
 # Create Azure AD application registration
-az ad app create --display-name "GitHub-To-Azure-Example" \
-  --query appId -o tsv
+az ad app create --display-name "GitHub-To-Azure-Example" --query appId -o tsv
 
 # Store the app ID in a variable
 APP_ID=$(az ad app list --display-name "GitHub-To-Azure-Example" --query [].appId -o tsv)
@@ -15,6 +14,16 @@ az ad app federated-credential create \
     "name": "github-oidc-branch",
     "issuer": "https://token.actions.githubusercontent.com",
     "subject": "repo:thomast1906/azure-oidc-example:ref:refs/heads/main",
+    "description": "GitHub Actions OIDC - Branch Workflows (main)",
+    "audiences": ["api://AzureADTokenExchange"]
+  }'
+
+az ad app federated-credential create \
+  --id $APP_ID \
+  --parameters '{
+    "name": "github-oidc-branch-renovate",
+    "issuer": "https://token.actions.githubusercontent.com",
+    "subject": "repo:thomast1906/azure-oidc-example:ref:refs/heads/renovate/configure",
     "description": "GitHub Actions OIDC - Branch Workflows (main)",
     "audiences": ["api://AzureADTokenExchange"]
   }'
